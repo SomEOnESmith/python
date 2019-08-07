4c00a41c78bd33ce024b6de18a655a8d0f1ddc69
Huusain_AlHaddad <h.ali.alhaddad@hotmail.com>
2019-08-08 00:54:50 +0430

    correct loops_task.py

--- loops_task.py
+++ loops_task.py
@@ -2,14 +2,15 @@ def calculate_total_price(price, quantity):
 	total_cost = price * quantity
 	return total_cost
 
-def sum_total(totals = [] , *args):
+def sum_total(totals):
 	sum = 0
 	for i in range(len(totals)):
-		sum += totals[i]
+		sum += totals[i]['total']
 	return sum
 
 
-cashier= {'item_name': [], 'price': [], 'quantity': [], 'total_price': []}
+cashier1= {'item_name': [], 'price': [], 'quantity': [], 'total_price': []}
+cashier = []
 
 
 while True:
@@ -17,19 +18,19 @@ while True:
 	if item_name.lower() == 'done':
 		break
 	else:
-		cashier['item_name'].append(item_name)
-		cashier['price'].append(float(input("Enter price:  ")))
-		cashier['quantity'].append(float(input("Enter quantity:  ")))
-		
+		item_price = float(input("Enter price:   "))
+		item_quantity = int(input("Enter quantity:  "))
+		item_total = calculate_total_price(item_price, item_quantity)
+		cashier.append({'name': item_name, 'price': item_price, 'quantity': item_quantity, 'total': item_total})
+
+print(cashier)	
 print("""---------------------------------------------------------
-	recepit
+recepit
 ---------------------------------------------------------""")
 
-#print (cashier)
-for i in range(len(cashier['item_name'])):
-	cashier['total_price'].append(calculate_total_price(cashier['price'][i],cashier['quantity'][i]))
-	print("%d %s %2.3f KD"%(int(cashier['quantity'][i]),cashier['item_name'][i],cashier['total_price'][i]))
+for item in cashier:
+	print("%d %s %2.3f KD"%(item['quantity'],item['name'], item['total']))
 
-total = sum_total(cashier['total_price'])
+total = sum_total(cashier)
 print("---------------------------------------------------------")
 print("Total = %2.3f KD"%(total))
